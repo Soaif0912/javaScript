8bc67d7f8972a703940105dbbbd997577b81b47d
Soaif Ikbal Mondal <107474186+Soaif0912@users.noreply.github.com>
2024-09-17 07:52:32 +0000

    complete this concept

--- this keyword/this.js
+++ this keyword/this.js
@@ -13,3 +13,73 @@ function x(){
 x();
 
 // inside function this doesn't have value. If the  strict mode is not on this refer to global object because of (this substituion), for strict mode it will return undefind.
+
+
+// also this value depends on how the function is called, if it's called with an object it behave differently.
+
+//window.x();  // where we get window object
+
+const student ={
+    name: "Soaif",
+    x : function(){          
+        console.log(this)
+    }
+}
+
+student.x();   
+
+// a function inside object is called method.
+// Here this will be the object which object method has called.
+
+// - call, apply and bind (sharing method);
+
+const student2 ={
+    name: "jojo"
+}
+
+student.x.call(student2);   // here this will be replaced with studen2
+
+// arrow function
+
+// Arrow functions don't have their own this. Instead, this refers to the value of this in the enclosing (lexical) context where the arrow function was defined.
+
+const person={
+    name: "soaif",
+    display: ()=>{
+        console.log(this);   // here this will be global object beacuse lexical scope is global
+    }
+}
+person.display();
+
+const person2={
+    name: "Samuel",
+    displayname: function(){
+        const y = () =>{
+            console.log(this);     
+        };
+        y();
+    }
+}
+person2.displayname();
+
+// here this will be the person2 object because y() arrow function lexical scope is displayname function
+
+
+// Inside a constructor function 
+
+// When used inside a constructor function, this refers to the new object that is being created.
+
+function human(name) {
+    this.name = name;
+  }
+  const human1 = new human('Alice');
+  console.log(human1.name);  // Logs "Alice"
+
+  // In Event handlers
+
+  // Inside an event handler, this refer to the element that received the event
+
+  document.querySelector(".button").addEventListener("click",function(){
+    console.log(this);   // Logs the button element
+  })
+  
